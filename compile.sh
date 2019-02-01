ebe6f275d451b06693ade1404660fc25ac1016d9
Jonas Enge <jonas.enge@t-fk.no>
2019-02-01 18:20:48 +0100

    Removes duplicate dependencies
    libSM-devel  libxslt-devel

--- compile.sh
+++ compile.sh
@@ -6,8 +6,8 @@ sudo yum install git autoconf ccache nasm libffi-devel libmpc-devel mpfr-devel \
 	gmp-devel libicu-devel icu python34-devel google-crosextra-caladea-fonts \
 	google-crosextra-carlito-fonts liberation-serif-fonts liberation-sans-fonts \
 	mesa-libGL-devel mesa-libGLU-devel libX11-devel libXext-devel libICE-devel \
-	libSM-devel libXrender-devel libxslt-devel gperf fontconfig-devel libpng-devel libxslt-devel \
-	expat-devel libcurl-devel nss-devel nspr-devel libSM-devel openssl-devel expat-devel.x86_64 -y
+	libSM-devel libXrender-devel libxslt-devel gperf fontconfig-devel libpng-devel \
+	expat-devel libcurl-devel nss-devel nspr-devel openssl-devel expat-devel.x86_64 -y
 sudo yum groupinstall "Development Tools" -y
 
 # clone libreoffice sources
